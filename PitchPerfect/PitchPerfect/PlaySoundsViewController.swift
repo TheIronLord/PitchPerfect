bdb6d3fe22de1e0fab0bbd7f0c86b78ea6e9b5d4
Sajjad Patel <sajjadp92@outlook.com>
2017-11-06 11:42:18 -0500

    Finished Implementing the AVAudioRecorderDelegate

---
+++ PitchPerfect/PitchPerfect/PlaySoundsViewController.swift
@@ -0,0 +1,21 @@
+//
+//  PlaySoundsViewController.swift
+//  PitchPerfect
+//
+//  Created by Sajjad Patel on 2017-11-06.
+//  Copyright © 2017 Sajjad Patel. All rights reserved.
+//
+
+import UIKit
+
+class PlaySoundsViewController: UIViewController {
+    
+    var recordedAudioURL: URL!
+    
+    override func viewDidLoad(){
+        
+    }
+    override func didReceiveMemoryWarning(){
+        
+    }
+}
